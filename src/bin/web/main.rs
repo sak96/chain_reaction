83c2742600dfdd16acedbb1b66fc30734da03e2d
sak96 <26397224+sak96@users.noreply.github.com>
2023-02-11 20:59:25 +0530

    Add click based events to the cells.

--- src/bin/web/main.rs
+++ src/bin/web/main.rs
@@ -11,37 +11,55 @@ pub struct CellProps {
     pub state: CellState,
     pub row: usize,
     pub col: usize,
+    pub player_count: u8,
+    pub onclick: Callback<(usize, usize), ()>,
 }
 
 #[function_component(Cell)]
 fn cell(
     CellProps {
         state,
-        row: _row,
-        col: _col,
+        row,
+        col,
+        onclick,
+        player_count,
     }: &CellProps,
 ) -> Html {
-    html! {
-        match state {
-            CellState::Explosion => "+1".to_string(),
-            CellState::NonEmpty(owner_id,atoms) => format!("{}{}", owner_id, atoms),
-            CellState::Empty => "\u{00a0}\u{00a0}".to_string(),
+    let row = *row;
+    let col = *col;
+    let (content, (h, s, l)) = match state {
+        CellState::Explosion => (0, (0, 100, 0)),
+        CellState::NonEmpty(owner_id, atoms) => {
+            let color = (
+                (((*owner_id as usize) * 360) / *player_count as usize),
+                50,
+                50,
+            );
+            (*atoms, color)
         }
+        CellState::Empty => (0, (0, 100, 100)),
+    };
+    html! {
+        <td style={format!("border: 1px solid black;background-color:hsl({}, {}%, {}%);",h,s,l)}
+            onclick={let onclick = onclick.clone(); move |_| { onclick.emit((row,col)) } }>
+            {content}
+        </td>
     }
 }
 
 #[function_component(App)]
 fn app(AppProps { players }: &AppProps) -> Html {
-    let board = use_mut_ref(|| Board::new(10, 10, *players));
+    let players = *players;
+    let board = use_mut_ref(|| Board::new(10, 10, players));
     let cells = {
-        let b= board.clone();
+        let b = board.clone();
         use_state_eq(|| b.borrow_mut().cells())
     };
     let onclick = {
         let b = board.clone();
         let cells = cells.clone();
         Callback::from(move |(r, c): (usize, usize)| {
-            let b: &mut Board =  &mut *b.borrow_mut();
+            let b: &mut Board = &mut *b.borrow_mut();
             if b.player_move(b.current_player_id(), r, c).is_ok() {
                 while b.next_iteration() {}
                 cells.set(b.cells());
@@ -56,13 +74,15 @@ fn app(AppProps { players }: &AppProps) -> Html {
                     |(r, row)| html!{<tr>{
                         row.iter().enumerate().map(
                             |(c, cell)|
-                            html!{<td style="border: 1px solid black;" onclick={
-                                let movement = onclick.clone();
-                                move |_| { movement.emit((r,c)) }
-                            }>
-                                // TODO: Fix cell clone please!!!
-                                <Cell state={cell.clone()} row={r} col={c}/>
-                            </td>}
+                            html!{
+                                <Cell
+                                    state={cell.clone()}
+                                    row={r}
+                                    col={c}
+                                    onclick={onclick.clone()}
+                                    player_count={players}
+                                />
+                            }
                         ).collect::<Html>()
                     }</tr>}
                 ).collect::<Html>()
@@ -72,5 +92,5 @@ fn app(AppProps { players }: &AppProps) -> Html {
 }
 
 fn main() {
-    yew::Renderer::<App>::with_props(AppProps { players: 2}).render();
+    yew::Renderer::<App>::with_props(AppProps { players: 2 }).render();
 }
