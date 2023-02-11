fb626521ac1caffc2dd32a443bd650574bc1161c
sak96 <26397224+sak96@users.noreply.github.com>
2023-02-11 16:39:25 +0530

    Move cell to it's own component

--- src/bin/web/main.rs
+++ src/bin/web/main.rs
@@ -1,28 +1,49 @@
-use yew::prelude::*;
 use chain_reaction::board::{Board, CellState};
+use yew::prelude::*;
 
 #[derive(Properties, PartialEq)]
 pub struct AppProps {
     pub board: Board,
 }
 
+#[derive(Properties, PartialEq)]
+pub struct CellProps {
+    pub state: CellState,
+    pub row: usize,
+    pub col: usize,
+}
+
+#[function_component(Cell)]
+fn cell(
+    CellProps {
+        state,
+        row: _row,
+        col: _col,
+    }: &CellProps,
+) -> Html {
+    html! {
+        match state {
+            CellState::Explosion => "+1".to_string(),
+            CellState::NonEmpty(owner_id,atoms) => format!("{}{}", owner_id, atoms),
+            CellState::Empty => "\u{00a0}\u{00a0}".to_string(),
+        }
+    }
+}
 
 #[function_component(App)]
-fn app(props: &AppProps) -> Html {
+fn app(AppProps { board }: &AppProps) -> Html {
     html! {
         <div style="display: flex;align-items: center;flex-direction: column;">
             <h1>{ "Chain Reaction" }</h1>
             <table style="border-collapse: collapse;font-size: 2.5em;">{
-                props.board.cells().iter().enumerate().map(
+                board.cells().iter().enumerate().map(
                     |(r, row)| html!{<tr>{
                         row.iter().enumerate().map(
-                            |(c, cell)| html!{<td style="border: 1px solid black;">{
-                                match cell {
-                                    CellState::Explosion => "+1".to_string(),
-                                    CellState::NonEmpty(owner_id,atoms) => format!("{}{}", owner_id, atoms),
-                                    CellState::Empty => "\u{00a0}\u{00a0}".to_string(),
-                                }
-                            }</td>}
+                            |(c, cell)|
+                            // TODO: Fix clone please!!!
+                            html!{<td style="border: 1px solid black;">
+                                <Cell state={cell.clone()} row={r} col={c}/>
+                            </td>}
                         ).collect::<Html>()
                     }</tr>}
                 ).collect::<Html>()
@@ -33,5 +54,5 @@ fn app(props: &AppProps) -> Html {
 
 fn main() {
     let board = Board::new(10, 10, 2);
-    yew::Renderer::<App>::with_props(AppProps{board}).render();
+    yew::Renderer::<App>::with_props(AppProps { board }).render();
 }
