e31b0c9230b4e0048437c9f86df558c518759918
sak96 <26397224+sak96@users.noreply.github.com>
2023-02-11 19:39:41 +0530

    Handle click events.

--- src/bin/web/main.rs
+++ src/bin/web/main.rs
@@ -3,7 +3,7 @@ use yew::prelude::*;
 
 #[derive(Properties, PartialEq)]
 pub struct AppProps {
-    pub board: Board,
+    pub players: u8,
 }
 
 #[derive(Properties, PartialEq)]
@@ -31,17 +31,36 @@ fn cell(
 }
 
 #[function_component(App)]
-fn app(AppProps { board }: &AppProps) -> Html {
+fn app(AppProps { players }: &AppProps) -> Html {
+    let board = use_mut_ref(|| Board::new(10, 10, *players));
+    let cells = {
+        let b= board.clone();
+        use_state_eq(|| b.borrow_mut().cells())
+    };
+    let onclick = {
+        let b = board.clone();
+        let cells = cells.clone();
+        Callback::from(move |(r, c): (usize, usize)| {
+            let b: &mut Board =  &mut *b.borrow_mut();
+            if b.player_move(b.current_player_id(), r, c).is_ok() {
+                while b.next_iteration() {}
+                cells.set(b.cells());
+            }
+        })
+    };
     html! {
         <div style="display: flex;align-items: center;flex-direction: column;">
             <h1>{ "Chain Reaction" }</h1>
             <table style="border-collapse: collapse;font-size: 2.5em;">{
-                board.cells().iter().enumerate().map(
+                cells.iter().enumerate().map(
                     |(r, row)| html!{<tr>{
                         row.iter().enumerate().map(
                             |(c, cell)|
-                            // TODO: Fix clone please!!!
-                            html!{<td style="border: 1px solid black;">
+                            html!{<td style="border: 1px solid black;" onclick={
+                                let movement = onclick.clone();
+                                move |_| { movement.emit((r,c)) }
+                            }>
+                                // TODO: Fix cell clone please!!!
                                 <Cell state={cell.clone()} row={r} col={c}/>
                             </td>}
                         ).collect::<Html>()
@@ -53,6 +72,5 @@ fn app(AppProps { board }: &AppProps) -> Html {
 }
 
 fn main() {
-    let board = Board::new(10, 10, 2);
-    yew::Renderer::<App>::with_props(AppProps { board }).render();
+    yew::Renderer::<App>::with_props(AppProps { players: 2}).render();
 }
